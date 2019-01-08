955138f7a63700e927930ed10e780b604cbec597
Stepan Vanecek <stepan.vanecek@cloudandheat.com>
2019-01-08 23:25:25 +0100

    improved algorithm, added plotting, params..

--- hw2/main.c
+++ hw2/main.c
@@ -5,6 +5,17 @@
 
 char * array;
 
+//init vars
+int array_sz = 1024 * 16384;
+int stride_min = 1;
+int stride_max = 600;
+int stride_step = 5;
+int repeat = 32;
+
+char * flush_array;
+int flush_array_sz = 10 << 20;
+
+
 static inline
 double gettime(void) {
 
@@ -13,56 +24,73 @@ double gettime(void) {
   return (tp.tv_sec * 1000000000 + tp.tv_nsec) >> 10;
 }
 
-int array_init(int array_size)
+char * array_init(int array_size)
 {
-  array = malloc(sizeof(char)*(array_size + 32));
+  char * arr = malloc(sizeof(char)*(array_size + 32));
   for(int i = 0; i< array_size; i++)
-    array[i] = rand()%256;
-  array[array_size] = '\0';
+    arr[i] = rand()%256;
+  arr[array_size] = '\0';
 
-  return 0;
+  return arr;
 }
+
 
 int main(int argc, char** argv)
 {
-  //init vars
-  int array_sz = 1024 * 8192;
-  int stride_min = 1;
-  int stride_max = 2500;
-  int stride_step = 25;
+  // PARAMS
+  //
+  // array_size
+  // stride_min
+  // stride_max
+  // stride_step
+  // repeat
 
-
-  if(array_init(array_sz))
+  if(argc > 2)
   {
-    printf("Error init array.\n");
-    return 1;
+    array_sz = atoi(argv[1]);
+    stride_min = atoi(argv[2]);
+    stride_max = atoi(argv[3]);
+    stride_step = atoi(argv[4]);
+    repeat = atoi(argv[5]);
   }
 
-  //printf("arrray:  %d %d %d %d \n", array[0], array[1], array[1024], array[1023]);
+  array = array_init(array_sz);
+  flush_array = array_init(flush_array_sz);
 
-  int arr_sum, remaining_steps;
-  double start_time, stop_time, abs_time;
+  int arr_sum, index;
+  double start_time, stop_time, abs_time, avg_time;
   int total_steps = array_sz / stride_max;
 
-  printf("array_sz %d total_steps: %d \n", array_sz, total_steps);
   //loop over strides
   for(int curr_stride = stride_min; curr_stride < stride_max; curr_stride += stride_step)
   {
-    remaining_steps = total_steps;
-    arr_sum = 0;
+    avg_time = 0;
 
-    start_time = gettime();
-    //access elems of array with stride i
-    for(int k = 0; k < array_sz; k += curr_stride)
+    for(int j = 0; j < repeat; j++)
     {
-      arr_sum += array[k];
-      if(--remaining_steps == 0)
-        break;
-    }
-    stop_time = gettime();
+      arr_sum = 0;
+      index = 0;
 
-    abs_time = stop_time - start_time;
-    printf("stride: %d, time: %f \n", curr_stride, abs_time);
+      start_time = gettime();
+      //access elems of array with stride i
+      for(int k = 0; k < total_steps; k++)
+      {
+        arr_sum += array[index];
+        index += curr_stride;
+      }
+      stop_time = gettime();
+
+      abs_time = stop_time - start_time;
+      avg_time += abs_time;
+
+      //flush data in the cache by loading another array
+      //do not to access each element, just need to load each cache line
+      for(int k = 0; k < flush_array_sz; k += 8)
+        arr_sum += flush_array[k];
+    }
+    avg_time = avg_time / repeat;
+
+    printf("stride: %d time: %f \n", curr_stride, avg_time);
 
 
   }
